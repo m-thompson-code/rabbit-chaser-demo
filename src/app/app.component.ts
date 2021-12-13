64683ac667e2d1c3259bb0a3b1b69bb0ae7bfb28
Mark Thompson <mthompson@bitovi.com>
2021-12-13 01:23:16 -0800

    Added testing against random rabbit with hints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RandomRabbitChaser } from "rabbit-chaser";
+
+declare const RC: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rabbit-chaser-demo';
+
+  rc: RandomRabbitChaser;
+
+  constructor() {
+    const index = Math.floor(Math.random() * 100);
+    this.rc = new RandomRabbitChaser(100, index);
+  }
+
+  guess(index: number): void {
+    this.getDistanceFromAnswer(index);
+    this.rc.checkIfRabbitAtIndex(index);
+    this.rc.getNextRabbitStates();
+  }
+
+  getDistanceFromAnswer(index: number): void {
+    const rabbitIndex = this.rc.getRabbitIndex();
+
+    const distance = Math.abs(rabbitIndex - index);
+
+    const maxIndex = this.rc.rabbitStates.length - 1;
+
+    if (distance > maxIndex * .9) {
+      console.log("ICE cold");
+      return;
+    }
+
+    if (distance > maxIndex * .7) {
+      console.log("freezing cold");
+      return;
+    }
+
+    if (distance > maxIndex * .5) {
+      console.log("cold");
+      return;
+    }
+
+    if (distance > maxIndex * .3) {
+      console.log("cold");
+      return;
+    }
+
+    if (distance > maxIndex * .1) {
+      console.log("hot");
+      return;
+    }
+
+    if (distance === 0) {
+      console.log("CAUGHT 'EM");
+      return;
+    }
+
+    console.log("BOILING hot");
+  }
 }
